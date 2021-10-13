d831fa8731826682cf244d1ae72775f717ba45cc
송재현 <rnswothd@naver.com>
2021-10-13 16:56:37 +0900

    Add files via upload

---
+++ prj_aop/src/main/java/org/ict/service/SampleSeviceImpl.java
@@ -0,0 +1,19 @@
+package org.ict.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleSeviceImpl implements SampleService{
+
+	@Override
+	public Integer doAdd(String str1, String str2) throws Exception {
+		return Integer.parseInt(str1) + Integer.parseInt(str2);
+	}
+
+	@Override
+	public void introduce() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
